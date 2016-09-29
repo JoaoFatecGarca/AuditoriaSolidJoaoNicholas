cc3698023b31ef5baf55690554e10c2779491881
JoaoFatecGarca <j-gamba@hotmail.com>
2016-09-29 09:16:24 -0300

    AuditoriaSolidJoaoNicholas

---
+++ WebLogin/App/Controller/AlunoController.js
@@ -0,0 +1,10 @@
+﻿var an = angular.module("app3")
+    .controller("AlunoController",
+        function validar() {
+            var ra;
+            ra = this.aluno.push({ ra: ra });
+            $http.get("http://localhost:49621/api/aluno")
+            .success(function (dados) {
+                $scope.retornoApi = dados;
+            })
+          });
